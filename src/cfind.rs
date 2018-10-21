6a528cdcdcb80e2570e4b79bb1abf1c55108b225
Chathura Colombage <dcdewaka@gmail.com>
2018-10-21 17:50:09 +0100

    command line options and regex based search

--- src/cfind.rs
+++ src/cfind.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::fs;
 use std::path::Path;
 use walkdir::WalkDir;
@@ -13,14 +14,41 @@ pub struct Searcher {
     config: Config,
 }
 
-fn parent_folder(path: &Path) -> Option<&Path> {
-    path.parent()
+struct Matcher {
+    exact_term: Option<String>,
+    regex_term: Option<Regex>,
 }
 
-fn is_root(path: &Path) -> bool {
-    match parent_folder(path) {
-        None => true,
-        Some(_) => false,
+impl Matcher {
+    fn matches(&self, path: &Path) -> bool {
+        match path.file_name() {
+            Some(fname) => {
+                let sname = fname.to_str().unwrap();
+
+                match self.regex_term {
+                    Some(ref re) => re.is_match(sname),
+                    None => match self.exact_term {
+                        Some(ref term) => sname == term,
+                        None => false,
+                    },
+                }
+            }
+            None => false,
+        }
+    }
+
+    fn new(term: &str, is_regex: bool) -> Matcher {
+        if is_regex {
+            Matcher {
+                regex_term: Some(Regex::new(term).unwrap()),
+                exact_term: None,
+            }
+        } else {
+            Matcher {
+                exact_term: Some(term.to_string()),
+                regex_term: None,
+            }
+        }
     }
 }
 
@@ -40,8 +68,10 @@ impl Searcher {
         let start_canonical = fs::canonicalize(start).unwrap();
         let mut current_path = Path::new(&start_canonical);
 
+        let matcher = Matcher::new(needle, self.config.regex);
+
         loop {
-            if self.find_in(&current_path, needle, &mut results) {
+            if self.find_in(&current_path, &matcher, &mut results) {
                 // We have found the needle, and we only have to continue if we
                 // need to find all possible matches
                 if !self.config.find_all {
@@ -50,13 +80,13 @@ impl Searcher {
             }
 
             // If we are already in the root directory, then we are done searching
-            if is_root(&current_path) {
+            if self.is_root(&current_path) {
                 break;
             }
 
             // Otherwise set the current_path to the one directory up and keep
             // searching
-            match parent_folder(&current_path) {
+            match current_path.parent() {
                 Some(path) => current_path = path,
                 None => {
                     // TODO: Better error handling
@@ -72,24 +102,14 @@ impl Searcher {
         results
     }
 
-    fn matches(&self, path: &Path, needle: &str) -> bool {
-        match path.file_name() {
-            Some(fname) => {
-                let sname = fname.to_str().unwrap();
-                sname == needle
-            }
-            None => false,
-        }
-    }
-
-    fn find_in(&self, path: &Path, needle: &str, results: &mut Vec<String>) -> bool {
+    fn find_in(&self, path: &Path, matcher: &Matcher, results: &mut Vec<String>) -> bool {
         let mut found = false;
 
         for entry in WalkDir::new(path).max_depth(1) {
             let current = entry.unwrap();
             let entry_path = current.path();
 
-            if self.matches(&entry_path, needle) {
+            if matcher.matches(&entry_path) {
                 found = true;
                 results.push(entry_path.to_str().unwrap().to_string());
 
@@ -100,5 +120,12 @@ impl Searcher {
         }
 
         found
+    }
+
+    fn is_root(&self, path: &Path) -> bool {
+        match path.parent() {
+            None => true,
+            Some(_) => false,
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,64 @@
+extern crate clap;
+extern crate regex;
 extern crate walkdir;
 
 mod cfind;
 
 use cfind::{Config, Searcher};
-
-// TODO: Command line option parsing
+use clap::{App, Arg};
 
 fn main() {
+    let matches = App::new("rename: bulk rename")
+        .version("0.1")
+        .author("Chathura C. <dcdewaka@gmail.com>")
+        .about("Find a file or directory by walking up parent directories")
+        .arg(
+            Arg::with_name("directory")
+                .short("d")
+                .required(false)
+                .takes_value(true)
+                .multiple(false)
+                .help("Directory to start search from. Default is the current directory."),
+        ).arg(
+            Arg::with_name("search")
+                .index(1)
+                .required(true)
+                .takes_value(true)
+                .help("Specify the directory to rename files in"),
+        ).arg(
+            Arg::with_name("exact")
+                .short("e")
+                .required(false)
+                .takes_value(false)
+                .multiple(false)
+                .help("Specify exact term search"),
+        ).arg(
+            Arg::with_name("all")
+                .short("a")
+                .required(false)
+                .takes_value(false)
+                .multiple(false)
+                .help("Specify whether to find all occurrences"),
+        ).get_matches();
+
+    let dir = matches.value_of("directory").unwrap_or(".");
+    let search = matches.value_of("search").unwrap();
+    let is_exact = matches.occurrences_of("exact") > 0;
+    let is_all = matches.occurrences_of("all") > 0;
+
     let config = Config {
-        find_all: true,
-        regex: false,
+        find_all: is_all,
+        regex: !is_exact,
     };
 
     let searcher = Searcher::new(config);
-    let res = searcher.search(".", "README.md");
+    let results = searcher.search(dir, search);
 
-    println!("Found results: {:?}", res);
+    if results.is_empty() {
+        std::process::exit(1);
+    }
+
+    for res in results {
+        println!("{}", res);
+    }
 }
